ffcf53dfdcf2c1b56b22c06f87ffefcc55856c79
andrew-curthoys <andrew@curthoys.io>
2021-04-14 19:50:46 -0500

    Added method for reading data from a csv file

--- app/utils/sqlite_helpers.py
+++ app/utils/sqlite_helpers.py
@@ -1,21 +1,45 @@
-import sqlite3
-from typing import Union
+from config import table_dict
 from os import PathLike
 from pathlib import Path
-from sqlite3 import Error
+from typing import Union
 from collections import namedtuple
-from config import table_dict
+from csv import reader as csv_reader
+import sqlite3
+from sqlite3 import Error
 
 class SQLUtil:
     def __init__(self, db_path: Union[str, PathLike]):
         self.db_path = db_path
+        self.data = []
 
 
-    def load_data(self, src: Union[str, PathLike], table: str):
-        # TODO: create namedtuple to store & load data to SQLite
+    def load_data(self, table: str):
+        # TODO: build SQL insert statement for inserting data
         pass
+    
+    def read_data(self, file_path: Union[str, PathLike], table: str):
+        """
+        Read data from a file & store in a data structure
+
+        Args:
+            file_path: file path for the data to be read
+            table: table where the data will be loaded
+        """
+        # Create a named tuple to store the data to upload into a table 
+        DataRow = namedtuple('DataRow', [key for key in table_dict[table]['cols']])
+        with open(file_path, newline='') as f:
+            reader = csv_reader(f, delimiter=',')
+            for row in reader:
+                data_row = DataRow(row)
+                self.data.append(data_row)
 
     def make_table(self, table: str):
+        """
+        Creates a table in the SQLite database
+
+        Args:
+            table: the name of the table to create
+        """
         col_data = table_dict[table]['cols']
         query = f'CREATE TABLE IF NOT EXISTS {table} ('
         stmt = []
