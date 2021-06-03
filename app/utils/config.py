cb3f3428156b348254ad1bb2fcd9700bede83934
andrew-curthoys <andrew@curthoys.io>
2021-06-02 21:47:35 -0500

    Added config info for buoy data

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,3 +1,26 @@
+year = 2021
+
+bouy_data_loc = {
+    'base_site': 'https://www.ndbc.noaa.gov/',
+    'standard_meterological_data': 'stdmet',
+    'spectral_wave_density_data': 'swden',
+    'spectral_wave_alpha1_direction_data': 'swdir',
+    'spectral_wave_alpha2_direction_data': 'swdir2',
+    'spectral_wave_r1_direction_data':'swr1',
+    'spectral_wave_r2_direction_data': 'swr2',
+    'supplemental_measurement_data': 'supl'
+}
+
+def buoy_data_months():
+    i = 0
+    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+              'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    while True:
+        yield months[i]
+        i += 1
+    
+snow_data_site = {}
+
 buoy_data = {
     'cols': {
         'STATION_ID': 'TEXT',
