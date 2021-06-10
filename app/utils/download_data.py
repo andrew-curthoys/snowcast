88e0ff6facdf6e923d2befed21ce06b2c7207371
andrew-curthoys <andrew@curthoys.io>
2021-06-09 22:37:06 -0500

    Wrote method to parse buoy data for writing to SQLite DB

--- app/utils/download_data.py
+++ app/utils/download_data.py
@@ -3,6 +3,7 @@ import chardet
 import config as cfg
 import gzip
 import os
+import re
 import sqlite3
 
 from io import BytesIO
@@ -18,8 +19,6 @@ class Downloader:
         Arguments:
             buoy_id: Identification number of the buoy
             report_type: Type of report that the buoy records, the types are listed in the README
-        Returns:
-            data: data from the buoy report
         """
         for i, mth in enumerate(cfg.buoy_data_months()):
             mth_int = i + 1
@@ -29,14 +28,30 @@ class Downloader:
                 break
             content = gzip.decompress(response.content)
             data_str = content.decode('ascii')
-            self.build_query(data_str, 'buoy_insert')
+            self.build_query(data_str, buoy_id)
     
 
-    def build_query(self, data_str: str, query_type: str):
-        data = data_str.split('/n')
-        pass
+    def build_query(self, data_str: str, buoy_id: str):
+        """Builds a query for inserting data into SQLite
+
+        Arguments
+            data_str: The data from the buoy in string format
+            buoy_id: The ID number of the buoy
+        """
+        data = data_str.split('\n')
+        query_vals = ''
+        for row in data[2:]:
+            row = row.strip()
+            print(row)
+            query_vals += '(' + buoy_id + ', ' + re.sub(' +', '', row[:16]) + ', ' + re.sub(' +', ', ', row) + '), '
+            print(query_vals)
 
     def write_to_db(self, query):
+        """Writes data to SQLite DB
+
+        Arguments:
+            query: The query to execute
+        """
         db_file = os.path.abspath('../../data/snowcast.db')
         with sqlite3.connect(db_file) as conn:
             cursor = conn.cursor()
